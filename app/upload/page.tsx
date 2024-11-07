4f3e9ccc0b318dde189a64f87bafb6518300c3b1
Paradox <chuhan718@gmail.com>
2024-11-07 08:16:22 +0530

    upload npm

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -1,17 +1,17 @@
-'use client'
+"use client";
 import { Container, Row, Col } from "react-bootstrap";
 const Upload = () => {
-    return ( 
-        <div className="">
-        <Container >
-          <Row>
-            <Col md={12}>
+  return (
+    <div className="">
+      <Container>
+        <Row>
+          <Col md={12}>
             <h1>Upload Page</h1>
-            </Col>
+          </Col>
         </Row>
       </Container>
     </div>
-     );
-}
- 
+  );
+};
+
 export default Upload;
